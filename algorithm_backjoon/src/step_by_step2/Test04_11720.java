cca8461be189fae69f15594b2350173a66ac5162
heeseonyang <yangvly1116@daum.net>
2021-08-31 21:23:26 +0900

    백준 다시 복습하기

--- algorithm_backjoon/src/step_by_step2/Test04_11720.java
+++ algorithm_backjoon/src/step_by_step2/Test04_11720.java
@@ -1,10 +1,23 @@
 package step_by_step2;
+//숫자의 합 - 다시
+import java.util.Scanner;
 
 public class Test04_11720 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		String a = scan.next();
+		scan.close();
+		
+		int sum = 0;
+		
+		for(int i=0; i<n; i++) {
+			sum += a.charAt(i)-'0';
+		}
 
+		System.out.println(sum);
 	}
 
 }
